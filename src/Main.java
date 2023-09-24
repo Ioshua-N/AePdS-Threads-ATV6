1626760de99f702585fe1bbe10db8cc8a86ece4e
Ioshua-N <ioshuanoliveira@gmail.com>
2023-09-24 16:29:30 -0300

    v1.1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        MyThread thread1 = new MyThread("Contagem 1", 0, 10);
-        MyThread thread2 = new MyThread("Contagem 2", 10, 50);
-        MyThread thread3 = new MyThread("Contagem 3", 50, 100);
+        MyThread thread1 = new MyThread("Contagem 1", 100, 300);
+        MyThread thread2 = new MyThread("Contagem 2", 301, 500);
+        MyThread thread3 = new MyThread("Contagem 3", 501, 700);
 
         thread1.start();
         thread2.start();
